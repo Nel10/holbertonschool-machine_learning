7936de295184123dedea04a2bd56a523499504f7
Nel10 <3461@holbertonschool.com>
2022-04-13 16:12:32 -0500

    file updated

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""""""
+"""Function cat_arrays"""
 
 
 def cat_arrays(arr1, arr2):
-    """"""
+    """Return concatenate arra"""
     new_list = []
     new_list = arr1 + arr2
     return new_list
