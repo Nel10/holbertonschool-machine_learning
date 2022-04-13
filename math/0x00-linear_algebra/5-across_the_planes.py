457f258492e652336a0426113350b84ba2685bc1
Nel10 <3461@holbertonschool.com>
2022-04-13 16:11:03 -0500

    file updated

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-""""""
+"""Function sum the matrix"""
 
 
 def add_matrices2D(mat1, mat2):
-    """"""
-    if len(mat1) != len(mat2):
+    """Return sum_matrix"""
+    if len(mat1[0]) != len(mat2[0]):
         return None
     sum_matrix = []
-    for i in range(len(mat1)):
-        sum_matrix.append(mat1[i], mat2[i])
+    for row in range(len(mat1)):
+        lit = []
+        for i in range(len(mat1[0])):
+            lit.append(mat1[row][i] + mat2[row][i])
+        sum_matrix.append(lit)
     return sum_matrix
