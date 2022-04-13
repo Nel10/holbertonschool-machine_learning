90ef9136675b13ea2b5c66f525886cd38be544b0
Nel10 <3461@holbertonschool.com>
2022-04-13 16:10:03 -0500

    file updated

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-""""""
+"""Function for sum list"""
+
 
 def add_arrays(arr1, arr2):
-    """"""
+    """return sum_list"""
     if len(arr1) != len(arr2):
         return None
     sum_list = []
