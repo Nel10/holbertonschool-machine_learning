d185037ef96ba12302598fad2580c6e1c2a9f84a
Nel10 <3461@holbertonschool.com>
2022-04-13 16:08:58 -0500

    file updated

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""""""
+"""Funtion for list matrix"""
 
 
 def matrix_transpose(matrix):
-    """"""
+    """Return list_matrix"""
     list_matrix = []
     new_matrix = zip(*matrix)
     for item in new_matrix:
