da9780e9fcff666fbc2ea66b523b71c9fb8eb23f
Nel10 <3461@holbertonschool.com>
2022-04-14 16:43:13 -0500

    file updated

--- math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
+++ math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
@@ -7,4 +7,4 @@ mat2 = matrix[:, 2:5]
 mat3 = matrix[1:, -3:]
 print("The middle two rows of the matrix are:\n{}".format(mat1))
 print("The middle two columns of the matrix are:\n{}".format(mat2))
-print("The bottom-right, square, 3x3T matrix is:\n{}".format(mat3))
+print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
