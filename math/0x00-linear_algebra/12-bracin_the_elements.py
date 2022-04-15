427c0e0255d24f51aa4bca72679688f42017bfd4
Nel10 <3461@holbertonschool.com>
2022-04-14 20:30:04 -0500

    performs

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
-""""""
+"""Function that performs operations"""
 
 
+def np_elementwise(mat1, mat2):
+    """Return operations"""
+    sum = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (sum, sub, mul, div)
