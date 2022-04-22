8d18153307b47fa2df1a8457c3833982a562c2ec
Nel10 <3461@holbertonschool.com>
2022-04-21 22:08:48 -0500

    function summation

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""return the summation_i_squared"""
+
+
 def summation_i_squared(n):
+    """Function return summation_i_squared"""
     if type(n) != int and n <= 0:
         return None
     return (n*(n + 1)*(2*n + 1))/6
