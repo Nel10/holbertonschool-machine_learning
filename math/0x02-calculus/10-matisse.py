81471507695e3dcad0851e7bb2b016e574b9f4b1
Nel10 <3461@holbertonschool.com>
2022-04-21 22:56:21 -0500

    file updated

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,10 +4,12 @@
 
 def poly_derivative(poly):
     """return derivate"""
-    if type(poly) is not list:
+    if type(poly) is not list or len(poly) == 0:
         return None
     derivate = []
     for i in range(len(poly)):
         if i >= 1:
             derivate.append(poly[i] * i)
+    if len(derivate) == 0:
+        derivate.append(0)
     return derivate
