41ac562c40752973d9f163ecd61e5419de255c68
Nel10 <3461@holbertonschool.com>
2022-05-10 15:08:01 -0500

    Calculate the value CDF with ERF

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -6,7 +6,7 @@ e = 2.7182818285
 
 def erf(x):
     """Return the error function (erf)"""
-    sumat = x - (x**3) / 3 + (x**5) / 10 + (x**7) / 42 + (x**9) / 216
+    sumat = x - (x**3) / 3 + (x**5) / 10 - (x**7) / 42 + (x**9) / 216
     ERF = (2 / (pi ** (0.5))) * sumat
     return ERF
 
