4f4c8712d0eee75857d113e43b83d283c4eba06d
Nel10 <3461@holbertonschool.com>
2022-05-10 12:26:16 -0500

    calculates the x-value and z-score

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -26,3 +26,13 @@ class Normal():
             for x in data:
                 sumat += (x - self.mean) ** 2
             self.stddev = (1 / len(data) * sumat) ** 0.5
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        z = (x - self.mean) / self.stddev
+        return z
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        x = self.stddev * z + self.mean
+        return x
