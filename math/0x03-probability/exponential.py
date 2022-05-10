9c0a207b7e9f6038301141c891fff4f60dd65369
Nel10 <3461@holbertonschool.com>
2022-05-09 22:58:44 -0500

    Create a class Exponential

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Create a class Exponential"""
+
+
+class Exponential():
+    """Represents an exponential distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        data is a list of the data
+        lambtha is the expected number
+        """
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(self.lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            self.lambtha = 1 / mean
