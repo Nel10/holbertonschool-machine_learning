53caff6b5f4fb82c47fb0a6dd65d0637fdb047b5
Nel10 <3461@holbertonschool.com>
2022-05-10 11:52:20 -0500

    Create a class Normal

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Create a class Normal"""
+
+
+class Normal():
+    """Represents a normal distribution"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        data is a list of the data
+        mean is the mean of the distribution
+        stddev is the standard deviation
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.stddev = float(stddev)
+            self.mean = float(mean)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            mean = sum(data) / len(data)
+            sumat = 0
+            for x in data:
+                sumat += (x - self.mean) ** 2
+            self.sttdev = (1 / len(data) * sumat) ** 0.5
