1f1a6e7b270cb52bf91057a6dc9544734e8995f1
Nel10 <3461@holbertonschool.com>
2022-05-11 00:29:15 -0500

    class Neuron that defines a single neuron

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""class Neuron that defines a single neuron"""
+import numpy as np
+
+
+class Neuron():
+    """
+    nx is the number of input features to the Neuron.
+    W: The weights vector for the Neuron.
+    b: The bias for the neuron.
+    A: The activated output of the neuron (prediction).
+    """
+
+    def __init__(self, nx):
+        """Initialize a class Neuron"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        # np.random.normal(loc=mean,scale=standard_deviation, size=samples)
+        # Loc: This parameter defaults to 0
+        # Scale: By default, the scale parameter is set to 1.
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
