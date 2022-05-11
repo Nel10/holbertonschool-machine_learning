e5474026a363642a4a4f9dd9e8a43f16fee32f96
Nel10 <3461@holbertonschool.com>
2022-05-11 00:50:52 -0500

    updated the simoid funtion

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""class Neuron that defines a single neuron"""
+import numpy as np
+
+
+class Neuron():
+    """
+    nx is the number of input features to the Neuron.
+    W: The weights vector for the Neuron.
+    b: The bias for the neuron.
+    A: The activated output of the neuron (prediction).
+    """
+
+    def __init__(self, nx):
+        """Initialize a class Neuron"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        # np.random.normal(loc=mean,scale=standard_deviation, size=samples)
+        # Loc: This parameter defaults to 0
+        # Scale: By default, the scale parameter is set to 1.
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter to Weight"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter to bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter to prediction"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        z = np.dot(X * W) + b
+        output = my_sigmoide(z)
+        self.__A = output
+        return self.__A
+
+
+def my_sigmoide(z):
+    """calculate a function sigmoid"""
+    return 1 / (1 + np.exp(-z))
