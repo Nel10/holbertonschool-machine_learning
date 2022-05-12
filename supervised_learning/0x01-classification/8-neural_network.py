3256ca7a7368cfbf20709d57ed68c4a6d1f24e09
Nel10 <3461@holbertonschool.com>
2022-05-12 16:14:13 -0500

    Create a class NeuralNetwork that defines a neural network

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-"""class NeuralNetwork that defines a neural network"""
+"""Create a class NeuralNetwork"""
 import numpy as np
 
 
 class NeuralNetwork():
     """
-    nx is the number of input features.
-    nodes is the number of nodes found in the hidden layer.
-    W1: The weights vector for the hidden layer.
-    b1: The bias for the hidden layer.
-    A1: The activated output for the hidden layer.
-    W2: The weights vector for the output neuron.
-    b2: The bias for the output neuron.
-    A2: The activated output for the output neuron (prediction).
+    class NeuralNetwork that defines a neural network
+    with one hidden layer performing binary classification
     """
 
     def __init__(self, nx, nodes):
-        """"""
+        """
+        nx is the number of input features.
+        nodes is the number of nodes found in the hidden layer.
+        W1: The weights vector for the hidden layer.
+        b1: The bias for the hidden layer.
+        A1: The activated output for the hidden layer.
+        W2: The weights vector for the output neuron.
+        b2: The bias for the output neuron.
+        A2: The activated output for the output neuron (prediction)
+        """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -26,8 +29,8 @@ class NeuralNetwork():
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         self.W1 = np.random.normal(size=(nodes, nx))
-        self.b1 = np.zeros(nodes, 1)
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        self.W2 = np.random.normal(1, nodes)
+        self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = 0
         self.A2 = 0
