ce8c03c40420872b090c0e98b017c3c84318ffad
Nel10 <3461@holbertonschool.com>
2022-05-12 18:39:00 -0500

    Calculates the cost of the model using logistic regression

--- supervised_learning/0x01-classification/11-neural_network.py
+++ supervised_learning/0x01-classification/11-neural_network.py
@@ -79,7 +79,7 @@ class NeuralNetwork():
     def cost(self, Y, A):
         """Calculates the cost of the model using logistic regression"""
         m = Y.shape[1]
-        cost = -(Y * np.log(A) + (1 - Y) * np.log(1.000001 - A)).sum() / m
+        cost = -((Y * np.log(A)) + (1 - Y) * np.log(1.000001 - A)).sum() / m
         return cost
 
 
