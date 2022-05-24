946af6a07b73981dd925f7723da1f9daaeaf713c
Nel10 <3461@holbertonschool.com>
2022-05-23 23:55:06 -0500

    Precision

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Precision for confusion matrix
+"""
+import numpy as np
+
+
+def precision(confusion):
+    """
+    precision for each class in a confusion matrix
+    Args:
+        confusion: (classes, classes)-where row indices represent the
+        correct labels and column indices represent the predicted labels
+            classes: is the number of classes
+    Returns:
+        (classes,)-containing the precision of each class
+    """
+    precision = np.zeros((confusion.shape[0]))
+    for x in range(confusion.shape[0]):
+        precision[x] = confusion[x][x]/np.sum(confusion[:,x])
+    return precision
