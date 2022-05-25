fd33e65b96eebcd168d0dfcd63748810c5d49bf3
Nel10 <3461@holbertonschool.com>
2022-05-25 00:01:25 -0500

    calculate loss

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -8,4 +8,4 @@ def calculate_loss(y, y_pred):
     y is a placeholder for the labels of the input data
     y_pred is a tensor containing the network’s predictions
     """
-    return tf.nn.softmax_cross_entropy_with_logits(y, y_pred)
+    return tf.nn.softmax_cross_entropy_with_logits_v2(y, y_pred)
