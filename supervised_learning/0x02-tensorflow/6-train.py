99b4447b926dcd861f2e1bf9b4cd8714a790df96
Nel10 <3461@holbertonschool.com>
2022-05-25 01:06:13 -0500

    fix

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -8,39 +8,42 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """Returns: the path where the model was saved"""
+def train(X_train, Y_train, X_valid,
+          Y_valid, layer_sizes, activations, alpha,
+          iterations, save_path="/tmp/model.ckpt"):
+    """Train Classification Model"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    tf.add_to_collection(name='x', value=x)
-    tf.add_to_collection(name='y', value=y)
-    prediction_total = forward_prop(x, layer_sizes, activations)
-    tf.add_to_collection(name='prediction', value=prediction_total)
-    loss_total = calculate_loss(y, prediction_total)
-    tf.add_to_collection(name='loss', value=loss_total)
-    accuracy = calculate_accuracy(y, prediction_total)
-    tf.add_to_collection(name='accuracy', value=accuracy)
-    train = create_train_op(loss_total, alpha)
-    tf.add_to_collection(name='train', value=train)
-
+    # Wrapper for Graph.add_to_collection() using the default graph.
+    # Stores value in the collection with the given name.
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    tf.add_to_collection('y_pred', y_pred)
+    loss = calculate_loss(y, y_pred)
+    tf.add_to_collection('loss', loss)
+    accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('accuracy', accuracy)
+    train_op = create_train_op(loss, alpha)
+    tf.add_to_collection("train_op", train_op)
+    # tf.global_variables_initializer()
+    # Returns an Op that initializes global variables.
     init = tf.global_variables_initializer()
-
-    with tf.Session() as session:
-        session.run(init)
+    # tf.Session: A class for running TensorFlow operations.
+    with tf.Session() as sess:
+        sess.run(init)
         for i in range(iterations + 1):
             if i % 100 == 0 or i == iterations:
                 print("After {} iterations:".format(i))
-                tloss, taccuracy = session.run([loss_total, accuracy],
-                                               feed_dict={x: X_train,
-                                                          y: Y_train})
+                tloss, taccuracy = sess.run([loss, accuracy],
+                                            feed_dict={x: X_train, y: Y_train})
                 print("\tTraining Cost: {}".format(tloss))
                 print("\tTraining Accuracy: {}".format(taccuracy))
-                vloss, vaccuracy = session.run([loss_total, accuracy],
-                                               feed_dict={x: X_valid,
-                                                          y: Y_valid})
+                vloss, vaccuracy = sess.run([loss, accuracy],
+                                            feed_dict={x: X_valid, y: Y_valid})
                 print("\tValidation Cost: {}".format(vloss))
                 print("\tValidation Accuracy: {}".format(vaccuracy))
             if i < iterations:
-                session.run(train, feed_dict={x: X_train, y: Y_train})
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+        # tf.train.Saver(): Saves and restores variables.
         saver = tf.train.Saver()
-        return saver.save(session, save_path)
+        return saver.save(sess, save_path)
