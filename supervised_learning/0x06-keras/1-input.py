137e52a3bdd013f68c03a24499053d038a96334a
Nel10 <3461@holbertonschool.com>
2022-05-30 18:02:28 -0500

    Build a neural network with Input class

---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""builds a neural network with the Keras library"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Args:
+        nx is the number of input features to the network
+        layers is a list containing the number of nodes in each
+        layer of the network
+        activations is a list containing the activation functions
+        used for each layer of the network
+        lambtha is the L2 regularization parameter
+        keep_prob is the probability that a node will be kept for dropout
+    Returns:
+        the keras model
+    """
+    regularizer = K.regularizers.L2(l2=lambtha)
+    for i in range(len(layers)):
+        if i == 0:
+            inputs = K.Input(shape=(nx, ))
+            prediction = K.layers.Dense(units=layers[i],
+                                        activation=activations[i],
+                                        kernel_regularizer=regularizer)(inputs)
+        else:
+            prediction = K.layers.Dense(units=layers[i],
+                                        activation=activations[i],
+                                        kernel_regularizer=regularizer)
+            (prediction)
+        if i < len(layers) - 1:
+            prediction = K.layers.Dropout(rate=(1 - keep_prob))(prediction)
+    model = K.Model(inputs=inputs, outputs=prediction)
+    return model
